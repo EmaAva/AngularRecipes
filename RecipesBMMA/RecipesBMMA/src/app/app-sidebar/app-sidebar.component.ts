29165f29c1e6a2b808b349f2146d0ee71c6081de
lucamarchisiooo <l.marchisio.1337@vallauri.edu>
2022-05-18 16:57:50 +0200

    sidebar fatta

--- RecipesBMMA/RecipesBMMA/src/app/app-sidebar/app-sidebar.component.ts
+++ RecipesBMMA/RecipesBMMA/src/app/app-sidebar/app-sidebar.component.ts
@@ -4,20 +4,18 @@ import { InsertServiceService } from '../insert-service.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './app-sidebar.component.html',
-  styleUrls: ['./app-sidebar.component.css']
+  styleUrls: ['./app-sidebar.component.css'],
 })
 export class AppSidebarComponent implements OnInit {
-  recipes:any;
-  constructor(private InsService:InsertServiceService) { }
+  recipes: any;
+  constructor(private InsService: InsertServiceService) {}
 
-  ngOnInit(): void {
-  }
-  
-  ViewRecipes(){
-    this.InsService.sendGetReq('/ViewRecipes').subscribe(
-      (data: any) => {
-        this.recipes = data;
-      }
-    );
+  ngOnInit(): void {}
+
+  ViewRecipes() {
+    this.InsService.sendGetReq('/ViewRecipes').subscribe((data: any) => {
+      this.recipes = data;
+      console.log(this.recipes);
+    });
   }
 }
